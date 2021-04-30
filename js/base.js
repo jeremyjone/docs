48fd89d67545716b9cc37e30ca6239aa3cc77c65
jeremyjone <jeremyjone@qq.com>
2021-04-30 12:26:15 +0800

    更新一个暗黑模式

---
+++ js/base.js
@@ -0,0 +1,10 @@
+if ($(".darkreader")[0]){
+    console.log("Darkreader Extension detected");
+} else {
+    DarkReader.setFetchMethod(window.fetch);
+    DarkReader.enable({
+         brightness: 100,
+         contrast: 90,
+         sepia: 10
+    });
+}
